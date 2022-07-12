f52e8e3620204c3490038f402f0bdab884776c9a
kuksag <georgiy.kuksa@gmail.com>
2022-07-12 15:52:26 +0300

    Return initial module-path

--- verification/src/http_server/routers/mod.rs
+++ verification/src/http_server/routers/mod.rs
@@ -1,24 +1,26 @@
-use paperclip::actix::web::ServiceConfig;
+use paperclip::actix::web::{self, ServiceConfig};
 
-use self::{solidity::SolidityRouter, sourcify::SourcifyRouter};
 pub use self::app::AppRouter;
+use self::{solidity::SolidityRouter, sourcify::SourcifyRouter};
 
 mod app;
 mod solidity;
 mod sourcify;
 
 pub trait Router {
-    fn register_routes(&self, service_config: &mut ServiceConfig);
+    fn register_routes(&self, service_config: &mut actix_web::web::ServiceConfig);
 }
 
 impl<T: Router> Router for Option<T> {
-    fn register_routes(&self, service_config: &mut ServiceConfig) {
+    fn register_routes(&self, service_config: &mut actix_web::web::ServiceConfig) {
         if let Some(router) = self {
             router.register_routes(service_config)
         }
     }
 }
 
-pub fn configure_router(router: &impl Router) -> impl FnOnce(&mut ServiceConfig) + '_ {
+pub fn configure_router(
+    router: &impl Router,
+) -> impl FnOnce(&mut actix_web::web::ServiceConfig) + '_ {
     |service_config| router.register_routes(service_config)
 }
